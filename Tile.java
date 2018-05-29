cc88168c878e402a48db35abbd2288dc41fb33a8
Chicken-Ninja <eko1@stuy.edu>
2018-05-28 23:25:14 -0400

    Apparantly my last commit didn't go through

---
+++ Tile.java
@@ -0,0 +1,80 @@
+public class Tile 
+{
+    Boolean hasPlayer,hasMonster,isWall;
+    int row;
+    int col; 
+    Monster monsterStore; 
+    Player playerStore;
+    
+    
+
+    public Tile(int x , int y , Boolean Wall) 
+    {
+	x = row; 
+	y = col; 
+	isWall = Wall;
+    }
+    
+    public Boolean isWall() 
+    {
+	return isWall;
+    }
+    
+    
+    
+    
+    public void PlayerOn(Player a) 
+    {
+	if(!(monsterStore) && playerStore == null && !(isWall))
+	    {
+		hasPlayer = true;
+		playerStore = a;
+	    }
+	
+    }
+    
+    public void MonsterOn(Monster a) 
+    {
+	if(!(playerStore) && monsterStore == null && !{isWall))
+	    {
+		hasMonster = true; 
+		monsterStore = a;
+	    }
+    }
+    
+    
+    public void removePlayer() 
+    {
+	if(!(isWall) && playerStore != null && hasPlayer)
+	    {
+		playerStore = null;
+		hasPlayer = false;
+	    }
+    }
+    
+    public void removeMonster() 
+    {
+	if(!(isWall) && monsterStore != null && hasMonster)
+	monster = null;
+	hasMonster = false; 
+    }
+    
+    public Boolean checkPlayer()
+    {
+	if(!(isWall))
+	    {
+		return hasPlayer();
+	    }
+    }
+
+    
+    public Boolean checkMonster()
+    {
+	if(!(isWall))
+	    {
+		return hasMonster();
+	    }
+    }
+}
+
+	
